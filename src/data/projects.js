2c75dedbf3dff0f70c7cd82f83b651c5fff13ff8
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-11-08 17:47:51 +0100

    [UPD] Optimized App.jsx's code with exports/imports

---
+++ src/data/projects.js
@@ -0,0 +1,43 @@
+import project1 from '../assets/progetto_1.png';
+import project2 from '../assets/progetto_2.png';
+import project3 from '../assets/progetto_3.png';
+import project4 from '../assets/progetto_4.png';
+import project5 from '../assets/progetto_5.png';
+
+export const projects = [
+    {
+        id: 1,
+        title: 'VideoGames E-Commerce',
+        desc: 'E-Commerce videoludica, lavoro di gruppo. Sistema gestionale di videogames, da aggiungere al carrello e tantissimo altro.',
+        href: 'https://github.com/Alessio-Gasbarro/ecommerce_boolstop',
+        image: project1
+    },
+    {
+        id: 2,
+        title: 'Javascript Clock',
+        desc: 'Orologio in tempo reale, con il design di una variabile JavaScript.',
+        href: 'https://github.com/Alessio-Gasbarro/realTimeClock_react',
+        image: project2
+    },
+    {
+        id: 3,
+        title: 'Nasa API',
+        desc: 'Applicazione che utilizza le API della NASA, mostra immagini giornaliere e informazioni di pianeti, asteroidi e altro.',
+        href: 'https://github.com/Alessio-Gasbarro/nasa_api',
+        image: project3
+    },
+    {
+        id: 4,
+        title: 'Pluvia24',
+        desc: 'Piattaforma meteo in tempo reale che utilizza le API di OpenWeather. Mostra le condizioni meteorologiche attuali di ben 20 Capitali.',
+        href: 'https://github.com/Alessio-Gasbarro/weather-api',
+        image: project4
+    },
+    {
+        id: 5,
+        title: 'Musicando',
+        desc: 'Applicazione per comparare strumenti e/o aggiungere strumenti ai preferiti. Ottimizzato con ricerca avanzata, debounce e LocalStorage.',
+        href: 'https://github.com/Alessio-Gasbarro/progetto-finale-spec-frontend-front',
+        image: project5
+    }
+]
